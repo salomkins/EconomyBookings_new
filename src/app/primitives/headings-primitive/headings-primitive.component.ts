993ba9215bd98fee41766878ac7e8d1032fc3e5b
Janis Salmins <salomkins@gmail.com>
2017-11-05 21:48:36 +0200

    Store button primitive expanded with input

--- src/app/primitives/headings-primitive/headings-primitive.component.ts
+++ src/app/primitives/headings-primitive/headings-primitive.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-headings-primitive',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadingsPrimitiveComponent implements OnInit {
 
+  private _usage = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get usage(): string {
+    return this._usage;
+  }
+
+  @Input('usage')
+  set usage(value: string) {
+    this._usage = value;
   }
 
 }
--- src/app/primitives/store-button-primitive/store-button-primitive.component.ts
+++ src/app/primitives/store-button-primitive/store-button-primitive.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-store-button-primitive',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreButtonPrimitiveComponent implements OnInit {
 
+  private _size = '';
+  private _brand = '';
+  private _color = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get size(): string {
+    return this._size;
+  }
+
+  @Input('size')
+  set size(value: string) {
+    this._size = value;
+  }
+
+  get brand(): string {
+  return this._brand;
+}
+
+  @Input('brand')
+  set style(value: string) {
+    this._brand = value;
+  }
+
+  get color(): string {
+    return this._color;
+  }
+
+  @Input('color')
+  set color(value: string) {
+    this._color = value;
   }
 
 }
