29573881f8e6158b833a6fa67c5b0f91369c5c94
Guntars V <v.guntars@inbox.lv>
2017-12-01 17:22:56 +0200

    pārliku src\app\primitives\input-primitive uz ng, sataisīju src\app\components\stay-tuned-block ieviesu iekš src\app\layouts\rent-car-section-layout

--- src/app/primitives/input-primitive/input-primitive.component.ts
+++ src/app/primitives/input-primitive/input-primitive.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-primitive',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputPrimitiveComponent implements OnInit {
 
+  private _styleInput = '';
+  private _typeInput = '';
+  private _nameInput = '';
+  private _placeholderInput = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get styleInput(): string {
+    return this._styleInput;
+  }
+  get typeInput(): string {
+    return this._typeInput;
+  }
+  get nameInput(): string {
+    return this._nameInput;
+  }
+  get placeholderInput(): string {
+    return this._placeholderInput;
+  }
+
+  @Input('styleInput')
+  set styleInput(value: string) {
+    this._styleInput = value;
+  }
+  @Input('typeInput')
+  set typeInput(value: string) {
+    this._typeInput = value;
+  }
+  @Input('nameInput')
+  set nameInput(value: string) {
+    this._nameInput = value;
+  }
+  @Input('placeholderInput')
+  set placeholderInput(value: string) {
+    this._placeholderInput = value;
   }
 
 }
