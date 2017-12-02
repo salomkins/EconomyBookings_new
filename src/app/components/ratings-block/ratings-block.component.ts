39311cd2e244a59e67e7ef5a1a6071038dc4bc01
Guntars V <v.guntars@inbox.lv>
2017-12-02 12:45:53 +0200

    Pārliku src\app\components\ratings-block uz ng

--- src/app/components/ratings-block/ratings-block.component.ts
+++ src/app/components/ratings-block/ratings-block.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ratings-block',
   templateUrl: './ratings-block.component.html',
   styleUrls: ['./ratings-block.component.scss']
 })
+
 export class RatingsBlockComponent implements OnInit {
+
+  private _ratingsList: [{ item: string, value: string }];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get ratingsList(): [{ item: string, value: string }] {
+    return this._ratingsList;
+  }
+  @Input('ratingsList')
+  set ratingsList(value: [{ item: string, value: string }]) {
+    this._ratingsList = value;
   }
 
 }
