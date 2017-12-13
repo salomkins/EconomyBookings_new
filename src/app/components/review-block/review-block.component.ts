f64f5d5614db04c9e283936e7f2340289a68f76d
Guntars V <v.guntars@inbox.lv>
2017-12-13 14:22:56 +0200

    Procesā......

--- src/app/components/review-block/review-block.component.ts
+++ src/app/components/review-block/review-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-review-block',
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewBlockComponent implements OnInit {
 
-  constructor() { }
+  _userData: {
+    name: string,
+    country: string,
+    countryCode: string,
+    date: string,
+    rating: number
+  };
+  _reviewsPlus: string[];
+  _reviewsMinus: string[];
 
+  constructor() { }
   ngOnInit() {
+  }
+
+  get userData(): {
+    name: string,
+    country: string,
+    countryCode: string,
+    date: string,
+    rating: number
+  } {
+    return this._userData;
+  }
+  @Input('userData')
+  set userData(value: {
+    name: string,
+    country: string,
+    countryCode: string,
+    date: string,
+    rating: number
+  }) {
+    this._userData = value;
+  }
+
+  get reviewsPlus(): string[] {
+    return this._reviewsPlus;
+  }
+  @Input('reviewsPlus')
+  set reviewsPlus(value: string[]) {
+    this._reviewsPlus = value;
+  }
+
+  get reviewsMinus(): string[] {
+    return this._reviewsMinus;
+  }
+  @Input('reviewsMinus')
+  set reviewsMinus(value: string[]) {
+    this._reviewsMinus = value;
   }
 
 }
