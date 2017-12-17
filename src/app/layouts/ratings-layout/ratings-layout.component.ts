2ef9da88eba4b725e2536b8aeda1c046ae2a8bf9
Guntars V <v.guntars@inbox.lv>
2017-12-17 13:29:58 +0200

    Procesā....

--- src/app/layouts/ratings-layout/ratings-layout.component.ts
+++ src/app/layouts/ratings-layout/ratings-layout.component.ts
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingsLayoutComponent implements OnInit {
 
-  constructor() { }
+rList = [
+  {'item' : 'Overall value for money of your car rental', 'value' : '8.3'},
+  {'item' : 'How easy was it to find pickup location?', 'value' : '8.2'},
+  {'item' : 'Work efficiency of the car rental agents', 'value' : '8.4'},
+  {'item' : 'Time spent picking up the car', 'value' : '8.5'},
+  {'item' : 'Time spent dropping off the car', 'value' : '7.9'},
+  {'item' : 'Car cleanliness', 'value' : '7.6'},
+  {'item' : 'Overall condition of the car', 'value' : '7.8'}
+];
 
+  constructor() { }
   ngOnInit() {
   }
 
